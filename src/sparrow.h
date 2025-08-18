f4a578cea2b416da8b4bf77393424572d64148de
Delon R. Newman <contact@delonnewman.name>
2025-08-18 19:25:58 -0400

    Enclose in parens

--- src/sparrow.h
+++ src/sparrow.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 #define IS_OBJECT(O) (O != NULL && O->sp_obj == true)
-#define TYPE_TAG_IS(O, T) O->type == T
+#define TYPE_TAG_IS(O, T) (O->type == T)
 #define IS_TYPE(O, T) IS_OBJECT(O) && TYPE_TAG_IS(O, T)
 
 enum {
