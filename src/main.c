fab3c9421ac2d5b9acdb3e99cc10e8f85f7c2d2d
Delon R. Newman <contact@delonnewman.name>
2025-08-25 22:55:54 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include "sparrow.h"
 
 int main() {
-  Object* obj = list_cons(object_float(1.1), list_cons(object_char('a'), list_cons(object_true(), list_cons(object_integer(2), list_cons(object_integer(1), list_empty())))));
+  say(list_empty());
+
+  Object* num = object_float(1.235);
+  say(num);
+
+  Object* obj = list_cons(
+    object_float(1.1),
+    list_cons(object_char('a'),
+              list_cons(object_true(),
+                        list_cons(object_integer(2),
+                                  list_cons(object_integer(1), list_empty())))));
   say(obj);
 
   Object* list1 = list_cons(object_integer(1), list_empty());
