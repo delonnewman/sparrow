9adcb77dbbf497755e566d302c532af80b5d069c
Delon R. Newman <contact@delonnewman.name>
2025-08-26 23:39:55 -0400

    Add SKIP macro

--- test/test_suite.h
+++ test/test_suite.h
@@ -1,6 +1,8 @@
 #include "sparrow.h"
 #include <assert.h>
 
+#define SKIP(x) NULL
+
 void assert_is_not_null(Object* obj);
 void assert_is_not_bool(Object* obj);
 void assert_is_not_text(Object* obj);
