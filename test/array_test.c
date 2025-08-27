e334babf14cbf728513882b5d12d9a577f6fa2c7
Delon R. Newman <contact@delonnewman.name>
2025-08-27 00:26:35 -0400

    Update array_at and array_set test

--- test/array_test.c
+++ test/array_test.c
@@ -8,19 +8,23 @@ void test_make_array() {
 }
 
 void test_array_at_and_set() {
-  Object* array = make_array(2);
+  Object* array = make_array(3);
 
+  // basics
   array_set(array, 0, object_integer(1));
   assert(is_equal(array_at(array, 0), object_integer(1)));
 
   array_set(array, 1, object_integer(2));
   assert(is_equal(array_at(array, 1), object_integer(2)));
 
+  // unset index
+  assert(is_equal(array_at(array, 2), object_null()));
+
   // out of range
   assert(is_equal(array_at(array, 2), object_null()));
 
   // reverse lookup
-  assert(is_equal(array_at(array, -1), object_integer(2)));
+  assert(is_equal(array_at(array, -2), object_integer(2)));
 }
 
 void test_array_at_empty() {
